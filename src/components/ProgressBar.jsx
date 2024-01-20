99b41dc50ed14c68ccc37063ce999143923e3df1
Sabina Zeynalova <117943057+ZSabina88@users.noreply.github.com>
2024-01-20 20:59:08 +0400

    final

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function ProgressBar({ timer }) {
+    const [remainingTime, setRemainingTime] = useState(timer);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log("inter");
+            setRemainingTime((prevTime) => prevTime - 10)
+        }, 10);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+    return (
+        <progress value={remainingTime} max={timer} />
+    );
+}
+
+export default ProgressBar;
